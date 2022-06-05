1882ef3c717c1ab495f39a4f21d5cb9b72792468
unknown <andrijgrineveckij@gmail.com>
2022-06-06 01:49:10 +0300

    task 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,11 +1,10 @@
+import Notiflix from 'notiflix';
+
 const refs = {
   form: document.querySelector('.form'),
-  delayInput: document.querySelector('input[name="delay"]'),
-  delayStepInput: document.querySelector('input[name="step"]'),
-  amountInput: document.querySelector('input[name="amount"]'),
-  submitBtn: document.querySelector('button[type="submit"]'),
 };
 
+const ERROR_MESSAGE = 'Enter values > 0 !!!';
 const formData = {};
 
 function createPromise(position, delay) {
@@ -36,19 +35,22 @@ function onInputChange(event) {
 function onFormSubmit(event) {
   event.preventDefault();
   let { delay, amount, step } = formData;
-  if (delay > 0 && amount > 0 && step > 0) {
-    for (let i = 1 && delay; i <= amount; i += 1) {
+  if (amount > 0 && step >= 0 && delay > 0) {
+    for (let i = 1; i <= amount; i += 1) {
       let position = i;
+      const promise = createPromise(position, delay);
       delay += step;
-      const promise = createPromise(position, delay)
+      promise
         .then(({ position, delay }) => {
-          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+          Notiflix.Notify.success(
+            `Fulfilled promise ${position} in ${delay}ms`
+          );
         })
         .catch(({ position, delay }) => {
-          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+          Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
         });
     }
   } else {
-    alert('Enter values > 0 !!!');
+    Notiflix.Notify.failure(ERROR_MESSAGE);
   }
 }
